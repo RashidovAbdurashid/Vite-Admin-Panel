6cc4dc89a560f6fb24d9302d3ca46b20df360b94
abdurashidhufjkl <abdurashidrashimov@gmail.com>
2026-06-23 16:54:06 +0500

    fix prob

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
-import Layout from "./Layout/Layout"
-import Home from "./pages/Home"
-import Profile from "./pages/Profile"
-import Users from "./pages/Users"
-import Product from "./pages/Product"
-import Carts from "./pages/Carts"
+import Layout from "./Layout/Layout";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import Users from "./pages/Users";
+import Product from "./pages/Product";
+import Carts from "./pages/Carts";
 import "./App.css";
 
 function App() {
-  return <div className="App">
-     
-  </div>;
+  return (
+    <div className="App">
+      <Home />
+      <Profile />
+      <Users />
+      <Product />
+      <Carts />
+    </div>
+  );
 }
 
 export default App;
