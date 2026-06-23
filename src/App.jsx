1947017c2f4296ac118a23e3f81f9970899aca03
abdurashidhufjkl <abdurashidrashimov@gmail.com>
2026-06-23 16:51:16 +0500

    start

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Layout from "./Layout/Layout"
+import Home from "./pages/Home"
+import Profile from "./pages/Profile"
+import Users from "./pages/Users"
+import Product from "./pages/Product"
+import Carts from "./pages/Carts"
+import "./App.css";
+
+function App() {
+  return <div className="App">
+     
+  </div>;
+}
+
+export default App;
